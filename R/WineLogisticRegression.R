f8ba681d629166a39723fd162f75553e3a246d9c
Fabio Pinheiro <fabiomgpinheiro@gmail.com>
2014-11-24 05:23:08 +0200

    Resultes Done

--- R/WineLogisticRegression.R
+++ R/WineLogisticRegression.R
@@ -26,15 +26,15 @@ ret1<-sum(round(predict(logi1, newdata= mytestdata[,-(13)]))==mytestdata[,12])/1
 print('Type')
 logi2<-glm(V14~fixedAcidity + volatileAcidity+citricAcid+ residualSugar+chlorides+freeSulfurDioxide+totalSulfurDioxide+density+pH+sulphates+alcohol,data=mydata[,-c(12)] )
 confint(logi2)
-aux<-round(predict(logi2, newdata= mytestdata[,-(12)]))
-ret2<-sum(aux==mytestdata[,14])/1000
+aux1<-round(predict(logi2, newdata= mytestdata[,-(12)]))
+ret2<-sum(aux1==mytestdata[,14])/1000
 
 #Quality With type
 print('Quality with type')
 logi3<-glm(quality~fixedAcidity + volatileAcidity+citricAcid+ residualSugar+chlorides+freeSulfurDioxide+totalSulfurDioxide+density+pH+sulphates+alcohol+V14,data=mydata[,-c(13)] )
 confint(logi3)
-aux<-round(predict(logi3, newdata= mytestdata[,-(13)]))
-ret3<-sum(aux==mytestdata[,12])/1000
+aux2<-round(predict(logi3, newdata= mytestdata[,-(13)]))
+ret3<-sum(aux2==mytestdata[,12])/1000
 
 print('Quality')
 ret1
@@ -43,13 +43,20 @@ ret2
 print('Quality with type')
 ret3
 
-
-matriz<-matrix(0, ncol=7,nrow=7)
+matriz1<-matrix(c(0,0,0,0),ncol=2,nrow=2)
 for (i in 1:1000) {
-  b<-aux[i]
-  a<-mytestdata[i,12]
-  matriz[a,b]<-matriz[a,b] +1
+  if(aux1[i]=='0'){b=1}else {b=2}
+  if(mytestdata[i,14]=='0'){a=1}else {a=2}
+  matriz1[a,b]<-matriz1[a,b] +1
 }
-matriz
-matriz[7,1]<-100
+matriz1
+
+matriz2<-matrix(0, ncol=7,nrow=7)
+for (i in 1:1000) {
+  b<-aux2[i]
+  a<-mytestdata[i,12]
+  matriz2[a,b]<-matriz2[a,b] +1
+}
+matriz2
+matriz2[7,1]<-100
 
